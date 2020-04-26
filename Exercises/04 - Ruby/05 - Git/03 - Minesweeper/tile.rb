f8f791eebd26102e77d7c3f6146a88643f68ac7a
William Akin <wakin66@gmail.com>
2020-04-26 07:09:05 -0400

    Built framework for Tile class

---
+++ Exercises/04 - Ruby/05 - Git/03 - Minesweeper/tile.rb
@@ -0,0 +1,31 @@
+class Tile
+
+    def initialize
+
+    end
+
+    def bombed?
+        @bomb
+    end
+
+    def flagged?
+        @flag
+    end
+
+    def revealed?
+        @revealed
+    end
+
+    def reveal
+
+    end
+
+    def neighbors
+
+    end
+
+    def neighbor_bomb_count
+
+    end
+
+end
