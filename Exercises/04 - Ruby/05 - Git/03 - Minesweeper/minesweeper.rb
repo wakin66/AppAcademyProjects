7120d1a473aa22bb8f578e7fba9188fd84d407eb
William Akin <wakin66@gmail.com>
2020-04-26 23:18:32 -0400

    Refactored creating new game via cli

--- Exercises/04 - Ruby/05 - Git/03 - Minesweeper/minesweeper.rb
+++ Exercises/04 - Ruby/05 - Git/03 - Minesweeper/minesweeper.rb
@@ -1,26 +1,37 @@
 require_relative 'args'
+require_relative 'board'
 
 class Game
 
-    def initialize(save = nil)
-
+    def initialize(file = nil,size = nil)
+        #Usage: #$0 ruby minesweeper.rb [-f arg] [-s arg]
+        if !size.nil?
+            @board = Board.new(size)
+        else
+            @board = Board.new
+        end
     end
 
     def play
-
+        board.render
+        pos = [2,2]
+        board[pos].reveal
+        puts
+        board.render
     end
+
+    private
+
+    attr_reader :board
 
 end
 
 if __FILE__ == $PROGRAM_NAME
-    flags = {           #List of possible cli flags and whether or not they accept another argument
-        "-f" => true    #This denotes a save file to be loaded.
+    flags = {           #Cli flags and whether or not they accept another argument
+        "-f" => true,   #This denotes a save file to be loaded.
+        "-s" => true    #Denotes size of board.
     }
     args = Args.new(ARGV,flags)
-    if args.include?("-f")
-        game = Game.new(args.value("-f"))
-    else
-        game = Game.new
-        game.play
-    end
+    game = Game.new(args["-f"],args["-s"])
+    game.play
 end
