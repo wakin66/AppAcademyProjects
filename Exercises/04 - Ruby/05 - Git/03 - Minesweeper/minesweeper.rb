77b90329bdff3b58182e47c48ff14bce11d74886
William Akin <wakin66@gmail.com>
2020-04-26 23:40:11 -0400

    Added methods to get a position from player

--- Exercises/04 - Ruby/05 - Git/03 - Minesweeper/minesweeper.rb
+++ Exercises/04 - Ruby/05 - Git/03 - Minesweeper/minesweeper.rb
@@ -14,10 +14,38 @@ class Game
 
     def play
         board.render
-        pos = [2,2]
+        pos = get_pos
         board[pos].reveal
         puts
         board.render
+    end
+
+    def get_pos
+        pos = nil
+        until pos && valid_pos?(pos)
+            puts "Please enter a position on the board (e.g., '3,4')"
+            print "> "
+
+            begin
+                pos = parse_pos(gets.chomp)
+            rescue
+                puts "Invalid position entered (did you use a comma?)"
+                puts ""
+
+                pos = nil
+            end
+        end
+        pos
+    end
+
+    def parse_pos(string)
+        string.split(",").map { |char| Integer(char) }
+    end
+
+    def valid_pos?(pos)
+        pos.is_a?(Array) &&
+            pos.length == 2 &&
+            pos.all? { |x| x.between?(0, board.size - 1) }
     end
 
     private
