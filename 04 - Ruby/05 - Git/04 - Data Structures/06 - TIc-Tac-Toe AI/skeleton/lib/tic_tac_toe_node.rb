f4f842e71144c9da463fbf4f86e1e53bd02b3004
William Akin <wakin66@gmail.com>
2020-05-01 17:38:10 -0400

    Wrote #children method

--- 04 - Ruby/05 - Git/04 - Data Structures/06 - TIc-Tac-Toe AI/skeleton/lib/tic_tac_toe_node.rb
+++ 04 - Ruby/05 - Git/04 - Data Structures/06 - TIc-Tac-Toe AI/skeleton/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +16,21 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    list = Array.new
+    (0..2).each do |x|
+      (0..2).each do |y|
+        pos = [x,y]
+        list << create_node(pos) if @board.empty?(pos)
+      end
+    end
+    return list
   end
+
+  def create_node(pos)
+    next_mover_mark == "X" ? mark = "O" : mark = "X"
+    next_board = @board.dup
+    next_board[pos] = next_mover_mark
+    return TicTacToeNode.new(next_board,mark,pos)
+  end
+
 end
