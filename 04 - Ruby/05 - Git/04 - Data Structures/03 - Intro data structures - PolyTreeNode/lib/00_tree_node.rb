ad0ea93cfb155a92068dcbc453a6a0fb830ce8fa
William Akin <wakin66@gmail.com>
2020-04-30 22:17:49 -0400

    Completed through phase 3

--- 04 - Ruby/05 - Git/04 - Data Structures/03 - Intro data structures - PolyTreeNode/lib/00_tree_node.rb
+++ 04 - Ruby/05 - Git/04 - Data Structures/03 - Intro data structures - PolyTreeNode/lib/00_tree_node.rb
@@ -8,15 +8,40 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        parent = node
-        parent.add_child(self) if !parent.nil?
+        return node if parent == node
+        old_parent = parent
+        @parent = node
+        node.add_child(self) if !node.nil?
+        old_parent.remove_child(self) if !old_parent.nil?
     end
 
     def add_child(child)
+        return child if children.include?(child)
         children << child
+        child.parent = self
     end
 
     def remove_child(child)
+        raise "Bad remove_child!" unless children.include?(child)
         children.delete(child)
+        child.parent = nil if child.parent == self
     end
+end
+
+def test
+    n1 = PolyTreeNode.new("root1")
+    n2 = PolyTreeNode.new("root2")
+    n3 = PolyTreeNode.new("root3")
+
+    # connect n3 to n1
+    n3.parent = n1
+    # connect n3 to n2
+    n3.parent = n2
+
+    # this should work
+    raise "Bad parent=!1" unless n3.parent == n2
+    raise "Bad parent=!2" unless n2.children == [n3]
+
+    # this probably doesn't
+    raise "Bad parent=!3" unless n1.children == []
 end
