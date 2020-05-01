ed63bbf90ca9afe1ca4115ff9ea7f445c92df66d
William Akin <wakin66@gmail.com>
2020-05-01 15:29:32 -0400

    Refactored ::valid_moves

--- 04 - Ruby/05 - Git/04 - Data Structures/04 - Knights Travails (Part One)/path_finder.rb
+++ 04 - Ruby/05 - Git/04 - Data Structures/04 - Knights Travails (Part One)/path_finder.rb
@@ -2,7 +2,7 @@ require_relative 'tree_node.rb'
 
 class KnightPathFinder
 
-    def initiate(start_pos)
+    def initialize(start_pos)
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
     end
@@ -17,25 +17,25 @@ class KnightPathFinder
 
     def self.valid_moves(pos)
         modifiers = [
-            [-1,-1],
-            [-1,0],
-            [-1,1],
-            [0,-1],
-            [0,1],
-            [1,-1],
-            [1,0],
-            [1,1]
+            [-2,-1],
+            [-2,1],
+            [-1,-2],
+            [-1,2],
+            [1,-2],
+            [1,2],
+            [2,-1],
+            [2,1]
         ]
         positions = Array.new
         modifiers.each do |mod|
             new_pos = [pos,mod].transpose.map {|x| x.reduce(:+)}
-            positions << new_pos if !new_pos.include?(-1) && !new_pos.include?(8)
+            positions << new_pos if new_pos.none? {|x| x < 0} && new_pos.none?{|x| x >= 8}
         end
         return positions
     end
 
     def new_move_positions(pos)
-        
+        KnightPathFinder.valid_moves(pos)
     end
 
 end
