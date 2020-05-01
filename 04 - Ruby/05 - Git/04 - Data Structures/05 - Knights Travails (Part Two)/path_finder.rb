1f28b32085cf681c11b984cec8544146ac0053fd
William Akin <wakin66@gmail.com>
2020-05-01 16:20:08 -0400

    Moved files for Part 2 of project

---
+++ 04 - Ruby/05 - Git/04 - Data Structures/05 - Knights Travails (Part Two)/path_finder.rb
@@ -0,0 +1,53 @@
+require_relative 'tree_node.rb'
+
+class KnightPathFinder
+
+    def initialize(start_pos)
+        @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
+    end
+    
+    def build_move_tree 
+        queue = [@root_node]
+        while !queue.empty?
+            start_node = queue.shift
+            positions = new_move_positions(start_node.value)
+            positions.each do |pos|
+                new_node = PolyTreeNode.new(pos)
+                new_node.parent = start_node
+                queue << new_node
+            end
+        end
+    end
+
+    def find_path(target_pos)
+
+    end
+
+    def self.valid_moves(pos)
+        modifiers = [
+            [-2,-1],
+            [-2,1],
+            [-1,-2],
+            [-1,2],
+            [1,-2],
+            [1,2],
+            [2,-1],
+            [2,1]
+        ]
+        positions = Array.new
+        modifiers.each do |mod|
+            new_pos = [pos,mod].transpose.map {|x| x.reduce(:+)}
+            positions << new_pos if new_pos.none? {|x| x < 0} && new_pos.none?{|x| x >= 8}
+        end
+        return positions
+    end
+
+    def new_move_positions(pos)
+        new_pos = KnightPathFinder.valid_moves(pos)
+        new_pos.delete_if {|ele| @considered_positions.include?(ele)}
+        @considered_positions += new_pos
+        return new_pos
+    end
+
+end
---
+++ 04 - Ruby/05 - Git/04 - Data Structures/05 - Knights Travails (Part Two)/tree_node.rb
@@ -0,0 +1,49 @@
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = Array.new
+        @value = value
+    end
+
+    def parent=(node)
+        return node if parent == node
+        old_parent = parent
+        @parent = node
+        node.add_child(self) if !node.nil?
+        old_parent.remove_child(self) if !old_parent.nil?
+    end
+
+    def add_child(child)
+        return child if children.include?(child)
+        children << child
+        child.parent = self
+    end
+
+    def remove_child(child)
+        raise "Bad remove_child!" unless children.include?(child)
+        children.delete(child)
+        child.parent = nil if child.parent == self
+    end
+
+    def dfs(target_value)
+        return self if value == target_value
+        children.each do |child|
+            result = child.dfs(target_value)
+            return result if !result.nil?
+        end
+        return nil
+    end
+
+    def bfs(target_value)
+        queue = Array.new
+        queue << self
+        while !queue.empty?
+            node = queue.shift
+            return node if node.value == target_value
+            node.children.each {|child| queue << child}
+        end
+        return nil
+    end
+end
