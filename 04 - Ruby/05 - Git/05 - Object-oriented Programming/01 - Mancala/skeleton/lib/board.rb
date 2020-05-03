a0a403f5bd4c62b6b868c808d5a91055192d9380
William Akin <wakin66@gmail.com>
2020-05-03 17:10:38 -0400

    Render board after every turn

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/01 - Mancala/skeleton/lib/board.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/01 - Mancala/skeleton/lib/board.rb
@@ -35,13 +35,14 @@ class Board
         num_stones -= 1
       end
     end
+    render
     return next_turn(idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
     return :prompt if ending_cup_idx == 6 || ending_cup_idx == 13
-    cups[ending_cup_idx].empty? ? :prompt : ending_cup_idx
+    cups[ending_cup_idx].empty? ? :switch : ending_cup_idx
   end
 
   def render
