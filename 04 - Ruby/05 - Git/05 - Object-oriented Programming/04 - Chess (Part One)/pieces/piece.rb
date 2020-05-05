811fb5d0624ecd41049f2ee685cbdf7a585ff41f
William Akin <wakin66@gmail.com>
2020-05-05 18:48:01 -0400

    Fixed #initialize and created skeleton for remainder of the class

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/pieces/piece.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/pieces/piece.rb
@@ -2,9 +2,35 @@ require_relative '../board'
 
 class Piece
 
-    def initialize(symbol,board,pos)
-        @symbol = symbol
+    def initialize(color,board,pos)
+        @color = color
         @board = board
         @pos = pos
+    end
+
+    def to_s
+
+    end
+
+    def empty?
+
+    end
+
+    def valid_moves
+
+    end
+
+    def pos=(val)
+
+    end
+
+    def symbol
+
+    end
+
+    private
+    
+    def move_into_check?(end_pos)
+
     end
 end
