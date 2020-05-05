e16e879c743b4c3457f98147fb552ae753f58ebd
William Akin <wakin66@gmail.com>
2020-05-05 18:21:58 -0400

    Added #valid_pos? and raising errors in #move_piece

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/board.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/board.rb
@@ -1,4 +1,5 @@
 require_relative 'pieces/piece'
+require_relative 'custom_errors'
 
 class Board
 
@@ -40,12 +41,18 @@ class Board
         @rows[x][y] = val
     end
 
-    def move_piece(color,start_pos,end_pos)
-
+    def move_piece(start_pos,end_pos) #add color argument back later
+        raise StartPositionError if !valid_pos?(start_pos)
+        raise EndPositionError if !valid_pos?(end_pos)
+        raise NoPieceError if self[start_pos] == nil
+        raise OccupiedPosition if self[end_pos] != nil
     end
 
     def valid_pos?(pos)
-
+        x,y = pos
+        return false if ![0,1,2,3,4,5,6,7].include? x
+        return false if ![0,1,2,3,4,5,6,7].include? y
+        return true
     end
 
     def add_piece(piece,pos)
