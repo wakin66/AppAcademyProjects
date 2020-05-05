51a8397064261b4f5b8062b5f794256557eddcea
William Akin <wakin66@gmail.com>
2020-05-05 16:55:22 -0400

    Initialize and fill chess board with generic pieces

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/board.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/board.rb
@@ -4,7 +4,30 @@ class Board
 
     def initialize
         @rows = Array.new(8) {Array.new(8)}
-        @sentinel = NullPiece.new
+        @sentinel = nil #change to NullPiece when class is written
+        fill_board
+    end
+
+    def fill_board
+        (0..7).each do |x|
+            (0..7).each do |y|
+                if [2,3,4,5].include? x #nullPieces
+                    @rows[x][y] = nil
+                elsif [1,6].include? x #Pawns
+                    @rows[x][y] = Piece.new
+                elsif [0,7].include? y #Rook
+                    @rows[x][y] = Piece.new
+                elsif [1,6].include? y #Knight
+                    @rows[x][y] = Piece.new
+                elsif [2,5].include? y #Bishop
+                    @rows[x][y] = Piece.new
+                elsif y == 3 #Queen
+                    @rows[x][y] = Piece.new
+                else #King
+                    @rows[x][y] = Piece.new
+                end
+            end
+        end
     end
 
     def [](pos)
