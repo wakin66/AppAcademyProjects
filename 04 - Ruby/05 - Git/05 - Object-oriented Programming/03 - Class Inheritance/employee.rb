3bf1872c0857283d75de03a4c18fad6b2428eced
William Akin <wakin66@gmail.com>
2020-05-05 14:24:55 -0400

    Allow setting of a manager and employees

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/03 - Class Inheritance/employee.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/03 - Class Inheritance/employee.rb
@@ -1,11 +1,18 @@
 class Employee
     attr_reader :name, :title, :salary, :boss
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary)
         @name = name
         @title = title
         @salary = salary
-        @boss = boss
+        @boss = nil
+    end
+
+    def boss=(manager)
+        return true if @boss == manager
+        @boss = manager
+        manager.add_employee(self)
+        return true
     end
 
 end
--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/03 - Class Inheritance/manager.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/03 - Class Inheritance/manager.rb
@@ -2,6 +2,23 @@ require_relative 'employee.rb'
 
 class Manager < Employee
 
-    def initialize()
+    def initialize(name,title,salary)
+        super
+        @employees = Array.new
+    end
+
+    def add_employee(employee)
+        return true if @employees.include?(employee)
+        @employees << employee
+        employee.boss = self
+        return true
+    end
+
+    def delete_employee(employee)
+        return true if !@employees.include?(employee)
+        @employees.delete(employee)
+        employee.boss = nil
+        return true
+    end 
 
 end
