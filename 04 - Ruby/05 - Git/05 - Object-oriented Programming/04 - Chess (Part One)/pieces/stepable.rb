0fe77bc9a3c134b631471906ae435ade9ecd2f81
William Akin <wakin66@gmail.com>
2020-05-07 07:13:51 -0400

    Implemented Stepable module

--- 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/pieces/stepable.rb
+++ 04 - Ruby/05 - Git/05 - Object-oriented Programming/04 - Chess (Part One)/pieces/stepable.rb
@@ -1,12 +1,18 @@
 module Stepable
 
     def moves
-
+        list = Array.new
+        move_diffs.each do |move|
+            next_move = [pos.first+move.first,pos.last+move.last]
+            list << next_move if board.valid_move?(next_move) && (board[new_move].empty? || board[new_move].color != color)
+        end
+        return list
     end
 
     private
 
     def move_diffs
-
+        #Overwritten by subclass
+        raise NotImplementedError
     end
 end
