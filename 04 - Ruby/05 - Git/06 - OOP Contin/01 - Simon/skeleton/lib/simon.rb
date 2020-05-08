16e62e58c69cc5ba80c81537614fcb49c0d88811
William Akin <wakin66@gmail.com>
2020-05-07 22:30:24 -0400

    Implemented all methods except for #require_sequence and #show_sequence needs improvement

--- 04 - Ruby/05 - Git/06 - OOP Contin/01 - Simon/skeleton/lib/simon.rb
+++ 04 - Ruby/05 - Git/06 - OOP Contin/01 - Simon/skeleton/lib/simon.rb
@@ -4,21 +4,35 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-    @sequence_length = 1
+    @sequence_length = Integer(1)
     @game_over = false
     @seq = Array.new
   end
 
   def play
-
+    while !game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !game_over
+      round_success_message
+      self.sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    seq.each.with_index do |color,idx|
+      print color
+      print " => " if (idx+1) != sequence_length
+    end
+    puts
   end
 
   def require_sequence
@@ -26,18 +40,20 @@ class Simon
   end
 
   def add_random_color
-
+    seq << COLORS[rand(COLORS.length)]
   end
 
   def round_success_message
-
+    puts "You did it! Next round..."
   end
 
   def game_over_message
-
+    puts "Congratulations, you survived for #{sequence_length} turns"
   end
 
   def reset_game
-
+    self.sequence_length = 1
+    self.game_over = false
+    seq.clear
   end
 end
