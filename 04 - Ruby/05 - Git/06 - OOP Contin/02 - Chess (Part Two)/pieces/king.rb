c6fe9f2c3eaa74e813391300f54b3ec9ba8b26f2
William Akin <wakin66@gmail.com>
2020-05-08 03:46:18 -0400

    Copied Chess files over to Part Two folder

---
+++ 04 - Ruby/05 - Git/06 - OOP Contin/02 - Chess (Part Two)/pieces/king.rb
@@ -0,0 +1,29 @@
+require_relative 'piece'
+require_relative 'stepable'
+require 'colorize'
+
+class King < Piece
+    include Stepable
+
+    def initialize(color,board,pos)
+        super
+    end
+
+    def symbol
+        '♚'.colorize(color)
+    end
+
+    def move_diffs
+        [
+        [-1,0],
+        [-1,1],
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1],
+        [0,-1],
+        [-1,-1]
+        ]
+    end
+
+end
