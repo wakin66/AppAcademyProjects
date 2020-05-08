0326e8e8296a8060b720974cf6e2d0c7e1979c7b
William Akin <wakin66@gmail.com>
2020-05-08 15:36:25 -0400

    Added @selected boolean and toggle on :return or :space

--- 04 - Ruby/05 - Git/06 - OOP Contin/02 - Chess (Part Two)/cursor.rb
+++ 04 - Ruby/05 - Git/06 - OOP Contin/02 - Chess (Part Two)/cursor.rb
@@ -32,11 +32,12 @@ MOVES = {
 
 class Cursor
 
-  attr_reader :cursor_pos, :board
+  attr_reader :cursor_pos, :board, :selected
 
   def initialize(cursor_pos, board)
     @cursor_pos = cursor_pos
     @board = board
+    @selected = false
   end
 
   def get_input
@@ -78,6 +79,7 @@ class Cursor
   def handle_key(key)
     case key
     when :return, :space
+      selected = selected ? false : true
       return cursor_pos
     when :left, :right, :up, :down
       update_pos(MOVES[key])
