c9f5dcb64d49942a8ce08332b96a122a0ea2ae0c
William Akin <wakin66@gmail.com>
2020-05-09 19:59:00 -0400

    Created HumanPlayer class

---
+++ 04 - Ruby/05 - Git/06 - OOP Contin/03 - Chess (Part Three)/players/human_player.rb
@@ -0,0 +1,43 @@
+require_relative 'player.rb'
+
+class HumanPlayer < Player
+
+    def initialize(color,display)
+        super
+    end
+
+    def make_move(board)
+        start_pos = nil
+        end_pos = nil
+
+        begin
+            until start_pos && end_pos
+                if start_pos
+                    display.render
+                    print "Where do you want to move the #{board[start_pos].class}?"
+                    until end_pos
+                        start_pos = display.cursor.get_input
+                        display.render
+                    end
+                else
+                    display.render
+                    print "What piece do you want to move?"
+                    until start_pos
+                        start_pos = display.cursor.get_input
+                        display.render
+                    end
+                end
+            end
+            board.move_piece(color,start_pos,end_pos)
+        rescue NoPieceError => e
+            puts e.message
+            start_pos = nil
+            retry
+        rescue EndPositionError => e
+            puts e.message
+            end_pos = nil
+            retry
+        end
+    end
+
+end
