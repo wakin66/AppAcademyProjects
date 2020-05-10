e50c3e32a87ef7405ddb47fe2ab3a9bbbd195b65
William Akin <wakin66@gmail.com>
2020-05-10 17:01:52 -0400

    Implemented #make_move and #get_piece

--- 04 - Ruby/05 - Git/06 - OOP Contin/03 - Chess (Part Three)/players/computer_player.rb
+++ 04 - Ruby/05 - Git/06 - OOP Contin/03 - Chess (Part Three)/players/computer_player.rb
@@ -7,7 +7,16 @@ class ComputerPlayer < Player
     end
 
     def make_move(board)
+        pieces = board.color_pieces(color)
+        piece = get_piece(pieces)
+        start_pos = piece.pos
+        end_pos = piece.valid_moves[rand(piece.valid_moves.count)]
+        board.make_move(color,start_pos,end_pos)
+    end
 
+    def get_piece(pieces)
+        valid_pieces = pieces.select {|piece| !piece.valid_moves.empty?}
+        valid_pieces[rand(valid_pieces.count)]
     end
 
 end
