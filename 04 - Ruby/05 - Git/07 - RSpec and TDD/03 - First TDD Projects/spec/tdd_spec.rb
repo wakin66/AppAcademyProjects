dbb05de1dc3d07547ffc89345ba46821eadd8d0e
William Akin <wakin66@gmail.com>
2020-05-21 00:07:35 -0400

    Removed unneeded code

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
@@ -1,17 +1,15 @@
 require 'tdd'
 
-#describe TDD do
-    describe "#my_uniq" do
-        it "doesn't raise an error when passed an array" do
-            expect {my_uniq([1,2,3])}.to_not raise_exception
-        end
-
-        it "raises an error if passed something other than an array" do
-            expect {my_uniq("not_array")}.to raise_exception(ArgumentError)
-        end
-
-        it "returns an array with no duplicates" do
-            expect(my_uniq([1,1,2,3])).to eq([1,2,3])
-        end
+describe "#my_uniq" do
+    it "doesn't raise an error when passed an array" do
+        expect {my_uniq([1,2,3])}.to_not raise_exception
     end
-#end
+
+    it "raises an error if passed something other than an array" do
+        expect {my_uniq("not_array")}.to raise_exception(ArgumentError)
+    end
+
+    it "returns an array with no duplicates" do
+        expect(my_uniq([1,1,2,3])).to eq([1,2,3])
+    end
+end
