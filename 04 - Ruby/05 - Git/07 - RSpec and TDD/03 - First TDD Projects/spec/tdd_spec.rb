c1fec6f954bafb5f23d562ea823070c5d253262a
William Akin <wakin66@gmail.com>
2020-05-21 19:44:54 -0400

    Wrote tests for Array#two_sums

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
@@ -13,3 +13,34 @@ describe "#my_uniq" do
         expect(my_uniq([1,1,2,3])).to eq([1,2,3])
     end
 end
+
+describe Array do
+    describe "#two_sum" do
+        it "returns an array" do
+            expect([-1, 0, 2, -2, 1].two_sum).to be_an(Array)
+        end
+
+        it "returns an array of pairs (2-D array)" do
+            expect([-1, 0, 2, -2, 1].two_sum.first).to be_an(Array)
+        end
+
+        it "returns pairs of indecies whose value sum to zero" do
+            arr = [-1, 0, 2, -2, 1]
+            pair = arr.two_sum.first
+            expect(arr[pair.first]+arr[pair.last]).to eq(0)
+        end
+
+        context "is sorted dictionary-wise" do
+            it "smaller first elements come first" do
+                arr = [-2,1,2,-1]
+                pairs = arr.two_sum
+                expect(pairs.first.first).to be < pairs.last.first
+            end
+            it "smaller second elements come first" do
+                arr = [1,-1,0,-1]
+                pairs = arr.two_sum
+                expect(pairs.first.last).to be < pairs.last.last
+            end
+        end
+    end
+end
