f113e385d2e2671de0b77850581e8e2f5dadb1f9
William Akin <wakin66@gmail.com>
2020-05-20 23:10:05 -0400

    Initial commit

---
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
@@ -0,0 +1,13 @@
+require 'tdd'
+
+describe TDD do
+    describe "#my_uniq" do
+        it "takes an array as a parameter"
+
+        it "raises an error if passed something other than an array"
+
+        it "returns an array with no duplicates"
+
+        it "returns a subarray of the parameter it was passed"
+    end
+end
