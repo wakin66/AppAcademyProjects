f366355cbecdf758d90bed4078fca6d702083225
William Akin <wakin66@gmail.com>
2020-05-20 23:34:34 -0400

    Wrote tests for #my_uniq

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/03 - First TDD Projects/spec/tdd_spec.rb
@@ -1,13 +1,22 @@
 require 'tdd'
 
-describe TDD do
+#describe TDD do
     describe "#my_uniq" do
-        it "takes an array as a parameter"
+        it "doesn't raise an error when passed an array" do
+            expect {my_uniq([1,2,3])}.to_not raise_exception
+        end
 
-        it "raises an error if passed something other than an array"
+        it "raises an error if passed something other than an array" do
+            expect {my_uniq("not_array")}.to raise_exception(ArgumentError)
+        end
 
-        it "returns an array with no duplicates"
+        it "returns an array with no duplicates" do
+            expect(my_uniq([1,1,2,3])).to eq([1,2,3])
+        end
 
-        it "returns a subarray of the parameter it was passed"
+        it "returns a subarray of the parameter it was passed" do
+            arr = [1,1,2,3]
+            expect(arr).to include(my_uniq(arr))
+        end
     end
-end
+#end
