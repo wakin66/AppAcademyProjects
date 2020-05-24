5aa258fb0871020091c8f79a8b8ba4f8c2ae2f59
William Akin <wakin66@gmail.com>
2020-05-24 15:04:22 -0400

    Created specs for Deck class

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/spec/deck_spec.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/spec/deck_spec.rb
@@ -1,30 +1,46 @@
 require 'deck'
 
 describe Deck do
-    let(:card) {double("card")}
+    let(:card) {double("Card")}
     subject(:deck) {Deck.new}
 
     context "with a new deck" do
         describe "#initialize" do
-            it "creates @cards as array"
-
-            it "fills @cards with cards"
-
-            it "fills @cards with 52 cards"
+            it "fills @cards with 52 cards" do
+                expect(deck.size).to eq(52)
+            end
         end
     end
 
     describe "#draw_card" do
-        it "removes the top card from the deck"
+        it "removes a card from the deck" do
+            deck.draw_card
+            expect(deck.size).to eq(51)
+        end
 
-        it "returns the top card from the deck"
+        it "returns the card removed from the deck" do
+            expect(deck.draw_card.class).to eq(Card)
+        end
     end
 
     describe "#shuffle" do
-        before(:each) {deck.shuffle}
-        it "reorders the @cards array"
+        it "reorders the @cards array" do
+            arr = deck.cards
+            deck.shuffle
+            expect(deck.cards).to match_array(arr)
+        end
 
-        it "doesn't add or remove cards"
+        it "doesn't add or remove cards" do
+            deck_length = deck.size
+            deck.shuffle
+            expect(deck.size).to eq(deck_length)
+        end
+    end
+
+    describe "#size" do
+        it "returns the length of the @cards array" do
+            expect(deck.size).to eq(52)
+        end
     end
 
 end
