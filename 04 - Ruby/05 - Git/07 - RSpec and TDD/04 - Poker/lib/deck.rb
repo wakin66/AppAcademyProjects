1f79f2b6f597236b821685fcc29d2e77fde6b0d8
William Akin <wakin66@gmail.com>
2020-05-24 15:11:04 -0400

    Implemented Deck#initialize and Deck#size

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/deck.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/deck.rb
@@ -2,6 +2,46 @@ require_relative 'card'
 
 class Deck
 
+    CARD_VALUES = [
+        :two,
+        :three,
+        :four,
+        :five,
+        :six,
+        :seven,
+        :eight,
+        :nine,
+        :ten,
+        :eleven,
+        :twelve,
+        :thirteen,
+        :fourteen
+    ]
 
+    CARD_SUITS = [
+        :Spades,
+        :Clubs,
+        :Diamonds,
+        :Hearts
+    ]
+
+    def initialize
+        @cards = Array.new
+        fill_cards
+    end
+
+    def size
+        return @cards.length
+    end
+
+    private
+
+    def fill_cards
+        CARD_SUITS.each do |suit|
+            CARD_VALUES.each do |value|
+                @cards << Card.new(value,suit)
+            end
+        end
+    end
 
 end
