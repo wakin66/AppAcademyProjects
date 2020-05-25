65bb2723b2edbeac6c9eb504263dbb843e37a59a
William Akin <wakin66@gmail.com>
2020-05-24 20:48:17 -0400

    Removed unnecisary code

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/card.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/card.rb
@@ -1,30 +1,14 @@
 class Card
     attr_reader :value, :suit
 
-    VALUES = {
-        :eleven => :Jack,
-        :twelve => :Queen,
-        :thirteen => :King,
-        :fourteen => :Ace
-    }
-
     def initialize(value,suit)
         raise(ArgumentError,"Invalid card value") unless value.is_a?(Symbol)
         raise(ArgumentError,"Invalid card suit") unless suit.is_a?(Symbol)
         @value = value
         @suit = suit
-        values_default
     end
 
     def to_s
-        return "#{VALUES[value]} of #{suit}"
-    end
-
-    private
-
-    def values_default
-        VALUES.default_proc = proc do |hash, key|
-            hash[key] = key
-        end
+        return "#{value} of #{suit}"
     end
 end
