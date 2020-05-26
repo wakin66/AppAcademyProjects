397dd2086f614876fa3c511c04549b6c914e1be4
William Akin <wakin66@gmail.com>
2020-05-25 22:31:20 -0400

    Completed Hand#initialize

--- 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/hand.rb
+++ 04 - Ruby/05 - Git/07 - RSpec and TDD/04 - Poker/lib/hand.rb
@@ -1,5 +1,98 @@
 require_relative 'deck'
 
 class Hand
+    attr_reader :cards, :hand
 
+    def initialize(deck)
+        @cards = Array.new
+        @deck = deck
+        fill_hand
+        @hand = generate_hand
+    end
+
+    def exchange_cards(arr)
+
+    end
+
+    def compare_hands(hand1,hand2)
+
+    end
+
+    private
+    attr_reader :deck
+
+    def fill_hand
+        5.times {cards << deck.draw_card}
+    end
+
+    def generate_hand
+        return :royal_flush if royal_flush?
+        return :straight_flush if straight_flush?
+        return :four_of_kind if four_of_kind?
+        return :full_house if full_house?
+        return :flush if flush?
+        return :straight if straight?
+        return :three_of_kind if three_of_kind?
+        return :two_pair if two_pair?
+        return :one_pair if one_pair?
+        return high_card
+    end
+
+    def royal_flush?
+
+    end
+
+    def straight_flush?
+
+    end
+
+    def four_of_kind?
+
+    end
+
+    def full_house?
+
+    end
+
+    def flush?
+        return true if cards.all? {|card| card.suit == :Spades}
+        return true if cards.all? {|card| card.suit == :Clubs}
+        return true if cards.all? {|card| card.suit == :Diamonds}
+        return true if cards.all? {|card| card.suit == :Hearts}
+        return false
+    end
+
+    def straight?
+
+    end
+
+    def three_of_kind?
+
+    end
+
+    def two_pair?
+
+    end
+
+    def one_pair?
+
+    end
+
+    def high_card
+        values = Array.new
+        cards.each {|card| values << card.value}
+        return :Ace if values.include?(:Ace)
+        return :King if values.include?(:King)
+        return :Queen if values.include?(:Queen)
+        return :Jack if values.include?(:Jack)
+        return :Ten if values.include?(:Ten)
+        return :Nine if values.include?(:Nine)
+        return :Eight if values.include?(:Eight)
+        return :Seven if values.include?(:Seven)
+        return :Six if values.include?(:Six)
+        return :Five if values.include?(:Five)
+        return :Four if values.include?(:Four)
+        return :Three if values.include?(:Three)
+        return :Two if values.include?(:Two)
+    end
 end
